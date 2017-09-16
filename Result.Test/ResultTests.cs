5b89c59e22709bc60ea1996cdf477242e0aed89b
Pawel  Jelinski <jelinski89@gmail.com>
2017-09-16 22:09:18 +0200

    added IsSuccess methode in Success class

--- Result.Test/ResultTests.cs
+++ Result.Test/ResultTests.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Result.Test
 {
+    public class ResultFactory
+    {
+        public static Success<T> CreateSuccess<T>(T value)
+        {
+            return new Success<T>(value);
+        }  
+    }
+
+    public interface Result
+    {
+        bool IsSuccess { get; }
+    } 
+
+    public class Success<T>: Result
+    {
+        public Success(T value)
+        {
+            IsSuccess = true;
+        }
+
+        public bool IsSuccess { get; }
+    }
+
     [TestFixture]
+    public class ResultTests
     {
         [Test]
+        public void GivenSuccess_IsSuccesShouldReturnTrue()
         {
-            Assert.True(true);
+            const string response = "test";
+            var result = ResultFactory.CreateSuccess(response);
+            Assert.That(result.IsSuccess, Is.True);
         }
     }
 }
