730a298783562f69a1c6fa49b0d00fe669036364
Pawel  Jelinski <jelinski89@gmail.com>
2017-09-17 22:47:15 +0200

    naive implementaion of Failure

--- Result.Test/ResultTests.cs
+++ Result.Test/ResultTests.cs
@@ -9,13 +9,30 @@ namespace Result.Test
         {
             return new Success<T>(value);
         }
+
+        public static Failure<T> CreateFailure<T>(string errorMessage)
+        {
+            return new Failure<T>(errorMessage);
+        }
     }
 
-    public interface Result<out T>
+    public interface Result<T>
     {
-        bool IsSuccess { get; }
-        T Unwrap();
+       bool IsSuccess { get; }
     }
+
+    public static class ResultExtensions
+    {
+        public static Success<T> ToSuccess<T>(this Result<T> result)
+        {
+            return (Success<T>)result;
+        }
+        
+        public static Failure<T> ToFailure<T>(this Result<T> result)
+        {
+            return (Failure<T>)result;
+        }
+    } 
 
     public class Success<T> : Result<T>
     {
@@ -35,27 +52,64 @@ namespace Result.Test
         }
     }
 
+    public class Failure<T>: Result<T>
+    {
+        private readonly string _erroMessage;
+
+        public Failure(string erroMessage)
+        {
+            IsSuccess = false;
+            _erroMessage = erroMessage;
+        }
+        
+        public bool IsSuccess { get; }
+        public string Unwrap()
+        {
+            return _erroMessage;
+        }
+    }
+
     [TestFixture]
     public class ResultTests
     {
-        public Result<string> ReturnSuccessWithString(string value)
-        {
-            return ResultFactory.CreateSuccess(value);
-        }
-
         [Test]
-        public void GivenSuccess_IsSuccesShouldReturnTrue()
+        public void GivenSuccess_IsSuccesShouldReturn_True()
         {
-            var result = ReturnSuccessWithString("test");
+            var result = ReturnSuccessWithString(string.Empty);
             Assert.That(result.IsSuccess, Is.True);
         }
-        
+
         [Test]
         public void GivenSuccess_UnwrapShouldReturn_test()
         {
             const string expected = "test";
-            var result = ReturnSuccessWithString(expected);
+            var result = ReturnSuccessWithString(expected).ToSuccess();
             Assert.That(result.Unwrap(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GivenFailure_IsSuccessShouldReturn_False()
+        {
+            var result = ReturnFailureWithString(string.Empty);
+            Assert.That(result.IsSuccess, Is.False);
+        }
+
+        [Test]
+        public void GivenFailure_UnwrapShouldReturn_test()
+        {
+            const string expected = "test";
+            var result = ReturnFailureWithString(expected).ToFailure();
+            Assert.That(result.Unwrap(), Is.EqualTo(expected));
+        }
+        
+        private static Result<string> ReturnSuccessWithString(string value)
+        {
+            return ResultFactory.CreateSuccess(value);
+        }
+
+        private static Result<string> ReturnFailureWithString(string value)
+        {
+            return ResultFactory.CreateFailure<string>(value);
         }
     }
 }
